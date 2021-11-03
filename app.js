27487b9a544fe76f1ca35229591fc01fc332071c
nagabandinikhil <43547232+nagabandinikhil@users.noreply.github.com>
2021-11-03 16:03:30 -0500

    changes made to all pages

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var icecreamRouter = require('./routes/icecream');
 var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector')
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
